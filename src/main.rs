b015de58d2ac47542f620874d9feaa3a3d2d4e5e
toxeeec <bartosz.kapciak@gmail.com>
2023-09-15 13:46:27 +0200

    feat: first square

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-use bevy::prelude::*;
+use bevy::{core_pipeline::tonemapping::Tonemapping, prelude::*};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(Camera3dBundle {
+        tonemapping: Tonemapping::None,
+        transform: Transform::from_xyz(5.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1. })),
+        material: materials.add(StandardMaterial {
+            base_color: Color::RED,
+            unlit: true,
+            ..Default::default()
+        }),
+        ..Default::default()
+    });
 }
