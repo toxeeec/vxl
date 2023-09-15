27f7298fa607b98b2dadc34f184fdb942802bced
toxeeec <bartosz.kapciak@gmail.com>
2023-09-15 14:34:48 +0200

    feat: add keyboard movement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use bevy::{core_pipeline::tonemapping::Tonemapping, prelude::*};
 
+const PLAYER_SPEED: f32 = 10.0;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, camera_movement)
         .run();
 }
 
@@ -18,7 +21,7 @@ fn setup(
         ..Default::default()
     });
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1. })),
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(StandardMaterial {
             base_color: Color::RED,
             unlit: true,
@@ -26,4 +29,35 @@ fn setup(
         }),
         ..Default::default()
     });
+}
+
+fn camera_movement(
+    mut query: Query<&mut Transform, With<Camera>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let mut camera_transform = query.get_single_mut().unwrap();
+    let mut direction = Vec3::ZERO;
+
+    if keyboard_input.pressed(KeyCode::W) {
+        direction += camera_transform.forward();
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        direction += camera_transform.back();
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        direction += camera_transform.left();
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        direction += camera_transform.right();
+    }
+    if keyboard_input.pressed(KeyCode::Space) {
+        direction += camera_transform.up();
+    }
+    if keyboard_input.pressed(KeyCode::ShiftLeft) {
+        direction += camera_transform.down();
+    }
+
+    let movement = direction.normalize_or_zero() * PLAYER_SPEED * time.delta_seconds();
+    camera_transform.translation += movement;
 }
