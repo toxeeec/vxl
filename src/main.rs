d7f1b4e40315a5ba4ce3e02049e3b3f69ba9e2e7
toxeeec <bartosz.kapciak@gmail.com>
2024-03-31 14:16:21 +0200

    feat: first block

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
-use bevy::prelude::*;
+use bevy::{core_pipeline::tonemapping::Tonemapping, prelude::*};
+use block::create_block_mesh;
+
+mod block;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    commands.spawn(Camera3dBundle {
+        tonemapping: Tonemapping::None,
+        transform: Transform::from_xyz(5.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    commands.spawn(MaterialMeshBundle {
+        material: materials.add(StandardMaterial {
+            base_color: Color::RED,
+            unlit: true,
+            ..Default::default()
+        }),
+        mesh: meshes.add(create_block_mesh()),
+        ..Default::default()
+    });
 }
